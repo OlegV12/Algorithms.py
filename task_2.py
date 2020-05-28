ed0cf691ec6dd4e2e404ea19c39a137efb896770
Oleg <olegvolobouev@gmail.com>
2020-05-29 00:35:18 +0300

    lesson-5

--- task_2.py
+++ task_2.py
@@ -1,67 +1,117 @@
-# Написать два алгоритма нахождения i-го по счёту простого числа.
-# Без использования решета Эратосфена;
-# С использованием решета Эратосфена.
-import itertools
-import timeit
-import cProfile
+# Написать программу сложения и умножения двух шестнадцатеричных чисел.
+# При этом каждое число представляется как коллекция, элементы которой это цифры числа.
+# Например, пользователь ввёл A2 и C4F. Сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
+# Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
+
+from collections import deque
 
 
-def prime_num(a):
-    counter = 0
-    while counter < a:
-        for i in itertools.count(2):
-
-            for j in range(2, i + 1):
-                if i % j == 0 and i != j:
-                    break
-                elif i % j == 0 and i == j:
-                    counter += 1
-            if counter == a:
-                return i
-
-
-def sieve_func(num, n=1000):
-    try:
-        sieve = [item for item in range(n)]
-        sieve[1] = 0
-
-        for item in range(2, n):
-            if sieve[item] != 0:
-                k = item + item
-                while k < n:
-                    sieve[k] = 0
-                    k += item
-
-        res = [item for item in sieve if item != 0]
-        result = res[num - 1]
-
-        return result
-    except IndexError:
-        n += 1000
-        return sieve_func(num, n)
+def equal_len(lst1, lst2):
+    """
+    Функция выравнивает длину списков.
+    :param lst1: list
+    :param lst2: list
+    :return: list, list
+    """
+    if len(lst1) > len(lst2):
+        while len(lst1) != len(lst2):
+            lst2.appendleft('0')
+    elif len(lst1) < len(lst2):
+        while len(lst1) != len(lst2):
+            lst1.appendleft('0')
+    return lst1, lst2
 
 
-w = prime_num(1000)
-q = sieve_func(1000)
-
-print(w, q)
-
-print(timeit.timeit("prime_num(10)", number=100, globals=globals()))  # 0.007315887999999937
-print(timeit.timeit("prime_num(100)", number=100, globals=globals()))  # 0.7552745570000001
-print(timeit.timeit("prime_num(1000)", number=100, globals=globals()))  # 123.15882781500001
-
-print(timeit.timeit("sieve_func(10)", number=100, globals=globals()))  # 0.08253115300000502
-print(timeit.timeit("sieve_func(100)", number=100, globals=globals()))  # 0.08032028399999547
-print(timeit.timeit("sieve_func(1000)", number=100, globals=globals()))  # 3.2133929059999957
-
-cProfile.run('prime_num(10)')    # 1    0.000    0.000    0.000    0.000 task_2.py:9(prime_num)
-cProfile.run('prime_num(100)')   # 1    0.007    0.007    0.007    0.007 task_2.py:9(prime_num)
-cProfile.run('prime_num(1000)')  # 1    1.294    1.294    1.294    1.294 task_2.py:9(prime_num)
-
-cProfile.run('sieve_func(10)')      # 1    0.001    0.001    0.001    0.001 task_2.py:25(sieve_func)
-cProfile.run('sieve_func(100)')     # 1    0.001    0.001    0.001    0.001 task_2.py:25(sieve_func)
-cProfile.run('sieve_func(1000)')  # 8/1    0.029    0.004    0.036    0.036 task_2.py:25(sieve_func)
+def list_sum(list1, list2):
+    """
+    Функция суммирует два списка по элементам и возвращает список.
+    :param list1: list
+    :param list2: list
+    :return: list
+    """
+    result_ = deque()
+    for x, y in zip(reversed(list1), reversed(list2)):
+        result_.appendleft(int(x, base=16) + int(y, base=16))
+    return result_
 
 
-# Вывод: функция с решетом Эратосфена эффективнее особенно если необходимо найти простые
-# числа с большим порядковым номером
+def sum_list_to_hex(lst):
+    """
+    Функция переводит список(результат от сложения 2х списков) в список шестнадцатиричных чисел.
+    :param lst: list
+    :return: list
+    """
+    lst.reverse()
+    for ind, item in enumerate(lst):
+        if item > 15:
+            try:
+                lst[ind + 1] += item // 16
+            except IndexError:
+                lst.append(item // 16)
+                lst[ind] = item % 16
+                break
+            lst[ind] = item % 16
+    lst.reverse()
+
+    for ind, itm in enumerate(lst):
+        lst[ind] = hex(itm)[2:].upper()
+    return lst
+
+
+def mul_func(num, lst):
+    """
+    Функция перемножает число num с каждым элементом списка lst.
+    :param num: int
+    :param lst: list
+    :return: list
+    """
+    mul_result = deque([])
+    num = int(num, base=16)
+    for i in lst:
+        e = num * int(i, base=16)
+        mul_result.append(e)
+    return mul_result
+
+
+user_num1 = input('Введите первое шестнадцатиричное число ')
+a = deque(list(user_num1))
+
+user_num2 = input('Введите второе шестнадцатиричное число ')
+b = deque(list(user_num2))
+print(*a)
+print(*b)
+
+if len(a) < len(b):
+    a, b = b, a
+
+res = []
+counter = len(b) - 1
+for _ in b:
+    v = mul_func(_, a)
+    for u in range(counter):
+        v.append(0)
+
+    res.append(v)
+    counter -= 1
+
+
+for ind, _ in enumerate(res):
+    res[ind] = sum_list_to_hex(_)
+
+for ind, _ in enumerate(res):
+    equal_len(res[ind], res[ind - 1])
+
+final = [0 for i in range(len(res[0]))]
+for i in range(len(res)):
+    final = list_sum(sum_list_to_hex(final), res[i])
+final = sum_list_to_hex(final)
+print('Произведение чисел: ')
+print(*final)
+
+print("*" * 20)
+
+equal_len(a, b)
+res = list_sum(a, b)
+result = sum_list_to_hex(res)
+print('Сумма чисел: ')
+print(*result)
